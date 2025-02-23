b03b9783fda43b437b941501c98380baac1f11fd
dganicolas <dganicolas@rafaelalberti.es>
2025-02-23 10:00:59 +0100

    primera idea- nico

---
+++ nico/configuracion.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+#importamos las librerías necesarias
+import sys
+import os
+from PyQt6.QtWidgets import * # Librerías de los componentes
+from PyQt6 import uic  # Librería para trabajar con el archivo de la interfaz
+import pyrebase
+import requests
+import json
+import BD
+import BD.basedatos
+from misviajes import MisViajes 
+
+#configuracion
+#eliminar la cuenta del usuario
+#actualizar la cuneta del usuario
+#si se actualiza la contraseña del mismo debemos actualizarloen el firebase,como se hace esto?
+#si se elimina la cuenta del usuario debemos eliminarlo del firebase,como se hace esto?
+#si se actualiza la cuenta del usuario debemos actualizarlo en el firebase,como se hace esto?
+#contraseña y email estan dentro del firebase, 
+#el usuario preguntar sobre el usuario una vez que se haya accedido 
+# en firebase trabajar directamente con usuario y cuenta, todo lo demas puede ser en bd local??
