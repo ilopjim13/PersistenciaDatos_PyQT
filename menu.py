485c63c36767b693435e1dbf31c89005c249f728
dganicolas <dganicolas@rafaelalberti.es>
2025-02-28 01:26:12 +0100

    nome entero de na

--- menu.py
+++ menu.py
@@ -18,11 +18,7 @@ class Menu(QtWidgets.QMainWindow):
         self.BMisViajes.clicked.connect(self.irAMisViajes)
         self.QTTredingsTopicsTabla.cellClicked.connect(self.irACompras)
         self.QLabelConfiguracion.mousePressEvent= self.mousePressEventLabel
-        if self.manager.usuario is not None:
-            self.refrescarIdDelLoginOResgiterDelCliente()
 
-    def refrescarIdDelLoginOResgiterDelCliente(self):
-        self.manager.usuario.id = baseLocal.obtenerElIdDelClienteUnaVezLogeado(self.manager.usuario.email)[0]
 
     def cargarDatos(self):
         destinos = baseLocal.obtenerSoloElNombreDelDestinoParaLaPantallaMenu()
