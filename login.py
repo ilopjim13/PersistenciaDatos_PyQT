6cb1bdf8adb221751b0aa87def764b867097f6ac
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2025-03-02 20:02:51 +0100

    Codigo documentado

--- login.py
+++ login.py
@@ -11,13 +11,15 @@ import models.cliente as model
 import BD.basedatos as baseLocal
 
 
-
+#Ventana Login / Register
 class Ventana(QMainWindow):
     def __init__(self,manager):
         super(Ventana, self).__init__()
+        #Buscamos la .ui
         file_log = "login.ui"
         full_path_lo = os.path.join(os.path.dirname(__file__), file_log)
         uic.loadUi(full_path_lo, self)
+        #Añadimos lso datos de firebase.
         self.manager = manager
         self.firebaseConfig = {
             "apiKey": "AIzaSyDtWlIUbITDQtvTI6pi4k0WxxOlimBIXxY",
@@ -29,45 +31,52 @@ class Ventana(QMainWindow):
             "appId": "1:827606316467:web:7cd3fcc0d0d4af6edec7c6"
         }
 
-        
+        #Nos autorzamos y conectamos con firebase
         self.firebase = pyrebase.initialize_app(self.firebaseConfig)
         self.auth = self.firebase.auth()
         self.db = self.firebase.database()
-
+        #Damos funcionalidad a los diferentes botones.
         self.bu_login.clicked.connect(self.gui_login)
         self.bu_reg.clicked.connect(self.gui_reg)
 
+    #Hacemos el register
     def gui_reg(self):
+        #Obtenemos los datos 
         nombre = self.li_nombre.text()
         apellido = self.li_apellido.text()
         dni = self.li_dni.text()
         email = self.li_usuario_2.text()
         passw = self.li_contra_2.text()
         
+        #Verificamos los datos vacíos
         if not (nombre and apellido and dni and email and passw):
             QMessageBox.critical(self, 'Error', "Ingrese todos los datos")
             return
-        
+        #Validamos el dni
         if not validar_dni(dni):
             QMessageBox.critical(self, 'Error', "El dni no es cumple el formato.")
             return
 
         try:
+            #Ingresamos el usuario en firebase
             user = self.auth.create_user_with_email_and_password(email, passw)
-
+            #Ingresamos los datos en la bd
             cliente = model.Cliente(0,nombre, email, apellido, dni)
             existo = baseLocal.insertar_cliente(cliente)
-
+            #Comprobamos que fue exitoso
             if existo is None:
                 QMessageBox.critical(self, 'Error', "No se ingresó el usuario en la base de datos")
 
             print(user)
+            #Relacionamos los diferentes datos 
             self.manager.usuario = cliente
             self.manager.mostrarVentana("menu")
             self.manager.token = user["idToken"]
             print(user["idToken"])
-
+            #Mandamos si fue correcto o no 
             self.correcto("r")
+
+        #Controlamos las excepciones
         except requests.exceptions.HTTPError as e:
             error_json = e.args[1]
             error = json.loads(error_json)['error']['message']
@@ -76,45 +85,53 @@ class Ventana(QMainWindow):
         except Exception as e:
             QMessageBox.critical(self, 'Error', "Error")
 
+    #Realizamos el login
     def gui_login(self):
+        #Obtenemos los datos
         email = self.li_usuario.text()
         passw = self.li_contra.text()
 
+        #Comprobamos si hay datos
         if not (email and passw):
             QMessageBox.critical(self, 'Error', "Ingrese todos los datos")
             return
         
         try:
+            #Comprobamos que esté en Firebase y la bd local
             user = self.auth.sign_in_with_email_and_password(email, passw)
             cliente = baseLocal.obtener_cliente(email)
 
             baseLocal.prueba()
-
+            
+            #Asignamos los datos y pasamos de pantalla
             self.manager.usuario = cliente   
             self.manager.mostrarVentana("menu")
             self.manager.token = user["idToken"]
 
+            #Pequeña comprobación del cliente
             if cliente is not None:
                 self.correcto("l")
             else:
                 QMessageBox.critical(self, 'Error', "No se encontró el usuario en la base de datos")
+        #Verificamos todos los posibles errores 
         except requests.exceptions.HTTPError as e:
             error_json = e.args[1]
             error = json.loads(error_json)['error']['message']
             QMessageBox.critical(self, 'Error', error)
             self.delete_line()
 
+    #Si los datos son correctos, enseñará diferentes mensajes.
     def correcto(self, estatus):
         if estatus == "l":
             QMessageBox.information(self, '¡Login correcto!', "Acceso garantizado")
         else:
             QMessageBox.information(self, '¡Usuario creado correctamente!', "Creación de usuario")
         
-
+    #Eliminamos los datos
     def delete_line(self):
         self.li_usuario.setText("")
         self.li_contra.setText("")
         self.li_usuario.setFocus()
-
+#Validamos el dni 
 def validar_dni(dni):
     return bool(re.fullmatch(r"\d{8}[A-Za-z]", dni))
--- misviajes.py
+++ misviajes.py
@@ -5,27 +5,31 @@ import sqlite3
 import BD.basedatos as baseLocal
 from datetime import datetime
 
+#Clase de La ventana
 class MisViajes(QtWidgets.QMainWindow):
     def __init__(self, manager):
         super().__init__()
         uic.loadUi("misviajes.ui", self) 
+        #añadimos la instancia del manager al self y comprobamos que existe.
         self.manager = manager
         if self.manager.usuario is not None:
+            #añadimostodos los datos necesarios a nuestra pantalla 
             self.email = self.manager.usuario.email
             self.boton_actualizar.clicked.connect(self.actualizar_viaje)
             self.boton_eliminar.clicked.connect(self.eliminar_viaje)
             self.boton_volver_menu.clicked.connect(self.volverMenu)
             self.cargar_viajes()
-
+    #Clase usada para cargar todos los viajes
     def cargar_viajes(self):
+        #Llamamos a la bd y obtenemos los datos.
         self.viajes = baseLocal.getMisViajes(self.email)
         print(f"Viajes encontrados: {self.viajes}") 
 
-        # La fuckin tabla
+        #Creamos la tabla con los headers correspondientes 
         self.tabla_viajes.setColumnCount(4)
         self.tabla_viajes.setHorizontalHeaderLabels(["Destino", "Fecha de Salida", "Fecha de Regreso", "Precio"])
         self.tabla_viajes.setRowCount(0) 
-
+        #Añadimos los datos a su celda
         for viaje in self.viajes:
             destino = viaje[2]
             fecha_salida = viaje[3]  
@@ -42,30 +46,27 @@ class MisViajes(QtWidgets.QMainWindow):
          
        
         
-
+    #Actualizamos las fechas ya que es lo unico que va ha poder actualizar.
     def actualizar_viaje(self):
+        #Miramos la fila seleccionada.
         selected = self.tabla_viajes.currentRow()
         if selected < 0:
             QtWidgets.QMessageBox.warning(self, "Error", "Selecciona un viaje para actualizar")
             return
-
+        #obtenemos el id
         viaje_id = int(self.viajes[selected][0])
 
-        fila = self.tabla_viajes.currentRow()
-        columna = self.tabla_viajes.currentColumn()
-
-        print(fila)
-        print(columna)
-
+        #Obtenemos la fechas
         nueva_fecha_salida = self.tabla_viajes.item(selected, 1).text() 
         nueva_fecha_regreso = self.tabla_viajes.item(selected, 2).text()
-
+        #Le damos el formato desado, ya que lo necesitamos en un formato concreto
         formato_fecha = "%Y-%m-%d"
         try:
+            #Ponemos los diferentes datos en su formato
             fecha_salida = datetime.strptime(nueva_fecha_salida, formato_fecha)
             fecha_regreso = datetime.strptime(nueva_fecha_regreso, formato_fecha)
             fecha_actual = datetime.today()
-
+            #Comprobamos los datos entre sí
             if fecha_salida < fecha_actual:
                 QtWidgets.QMessageBox.warning(self, "Error", "La fecha de salida no puede ser en el pasado.")
                 return
@@ -73,32 +74,34 @@ class MisViajes(QtWidgets.QMainWindow):
             if fecha_regreso < fecha_salida:
                 QtWidgets.QMessageBox.warning(self, "Error", "La fecha de regreso no puede ser anterior a la de salida.")
                 return
-
+        #Excepción para posible error del formato
         except ValueError:
-            QtWidgets.QMessageBox.warning(self, "Error", "Formato de fecha inválido. Use YYYY-MM-DD.")
+            QtWidgets.QMessageBox.warning(self, "Error", "Formato de fecha inválido.")
             return
-
+        #Cargamos en la bd
         baseLocal.putMisViajes(nueva_fecha_salida, nueva_fecha_regreso,viaje_id)
-
+        #Recargamos la ventana para ver el cambio
         self.cargar_viajes()
 
+    #Eliminamos los viajes
     def eliminar_viaje(self):
-
+        #Seleccionamos la fila
         selected = self.tabla_viajes.currentRow()
         if selected < 0:
             QtWidgets.QMessageBox.warning(self, "Error", "Selecciona un viaje para eliminar")
             return
-        
+        #Obtenemos el id
         viaje_id = int(self.viajes[selected][0])
-
+        #Confirmamos la orden ya que se eliminarán datos
         respuesta = QtWidgets.QMessageBox.question(
             self, "Confirmar Eliminación", "¿Estás seguro de eliminar este viaje?",
             QtWidgets.QMessageBox.StandardButton.Yes | QtWidgets.QMessageBox.StandardButton.No
         )
+        #En caso de afirmación se elimina y recargamos la ventana.
         if respuesta == QtWidgets.QMessageBox.StandardButton.Yes:
             baseLocal.delMisViajes(viaje_id)
             self.cargar_viajes()
     
-    
+    #Volvemos al menu.
     def volverMenu(self):
         self.manager.mostrarVentana("menu")
